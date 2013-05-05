82d8c2d5ba08a5b11fcf9794425ac30167f391c2
Ron De Benedetti <RonDeBen@gmail.com>
2013-05-05 15:32:09 -0500

    now accepts args for Kenneth

--- src/GraphViz.java
+++ src/GraphViz.java
@@ -6,19 +6,30 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 
-public class main {
+public class GraphViz {
 
     private static PrintWriter outFile;
-    
+    private String curriculum, transcript, outName;
     public static void main(String[] args) throws IOException {
         
-        Scanner fileScan = new Scanner(new File("curriculum.txt"));
-        outFile = new PrintWriter (new FileWriter ("transcript.dot"));
+        GraphViz program = new GraphViz(args);
+        program.run();
+    }
+    
+    private GraphViz(String[] args){
+        this.curriculum = args[0];
+        this.transcript = args[1];
+        this.outName = args[2];
+    }
+    
+    private void run() throws IOException{
+        Scanner fileScan = new Scanner(new File(curriculum+".txt"));
+        outFile = new PrintWriter (new FileWriter (outName+".dot"));
         
         Courses c = new Courses(fileScan);
         DotWriter dw = new DotWriter(c);
         
-        fileScan = new Scanner(new File("transcript.txt"));
+        fileScan = new Scanner(new File(transcript+".txt"));
         
         c.read(fileScan);
         c.findAvailableClasses();
@@ -32,3 +43,4 @@ public class main {
         outFile.close();
     }
 }
+
